c168a5f1653b8c05cf469b5c08d10fc2dd23f5cd
David <david.chalyan25@gmail.com>
2022-08-18 15:17:30 +0300

    Добавьте файлы проекта.

---
+++ ManageDBApp/Model/DataWorker.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManageDBApp.Model
+{
+    public static class DataWorker
+    {
+        // создать отдел
+
+        // создать позицию
+
+        // создать сотрудника
+
+        // удаление отдела
+
+        // удаление позицию
+
+        // удаление сотрудника
+
+        // редактирование отдела
+
+        // редактирование позицию
+
+        // редактирование сотрудника
+    }
+}
